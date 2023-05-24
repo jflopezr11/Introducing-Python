730f2317bab593b471c026e04352bbfce6ad77bf
Joshua Lopez <joshualopezroque@gmail.com>
2023-05-23 17:44:04 -0700

    finished chapter 6

--- intro_python_ch_6.py
+++ intro_python_ch_6.py
@@ -54,3 +54,62 @@ for letter in word2:
     if letter == 'u': 
         break
     print(letter)
+
+
+#check break Use with else
+
+#just like 'while', 'for' also has an optional else that checks wherther the 'for' completed normally. if break was not called, the else statement is run. 
+
+word = 'thud'
+for letter in word: 
+    if letter == 'x':
+        print("Eek! And 'x'")
+        break
+    print(letter)
+else: 
+    print("No 'x' in there.")
+
+
+
+#6.3 Generate Number Sequences with range()
+for x in range(0,3): 
+    print(x)
+
+#6.1 Use a for loop to print the values of the list [3,2,1,0]
+
+new_list = [3,2,1,0]
+
+for number in new_list:
+    print(number)
+
+#6.2 Assign the value 7 to the variable guess_me, and the value 1 to the variable number. Write a while loop that compares number with guess_me. Print 'too low' if number is less than guess_me.
+# If number equals guess_me, print "found it!" and then exit the loop. if number is greater than guess_me, print 'opps' and then exit the loop. Increment number at the end of the loop. 
+
+guess_me = 7
+number = 1
+
+while True: 
+    if number < guess_me:
+      print('too low')
+      print(number) #added this because I would like to see my loop work. 
+    elif number == guess_me:
+        print("found it!")
+        break
+    elif number > guess_me: 
+        print("opps!")
+        break
+    number+=1 
+
+#6.3 Assign the value 5 to the variable guess_me. Use a for loop to iterate a variable called number over range(10). if number is less than guess_me, print 'too low'. if it equals guess_me, print "found it!" and then break out of the for loop. 
+#if number is greater than gues_me, print "opps" and then exit the loop. 
+
+guess_me_two = 5
+
+for number in range(10): 
+    if number < guess_me_two: 
+        print("too low!")
+    elif number == guess_me_two: 
+        print('found it!')
+        break
+    else: 
+        print("oops!")
