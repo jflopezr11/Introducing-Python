5a346eaa514303c6362f91649cfe4ecd0493936d
Joshua Lopez <joshualopezroque@gmail.com>
2023-07-22 22:25:25 -0700

    started chapter 8

---
+++ intro_python_ch_8.py
@@ -0,0 +1,14 @@
+## Dictionaries and Sets
+
+## Dictionaries are similar to a list, but the order of the items doesn't matter, and they aren't selected by an offset such as 0 or 1. Instead, you specify a unique key to associate with each value. 
+
+## Dictionary created by {}
+empty_dict = {}
+
+val_agents = {
+    "Jett":"uses knifes and wind!", 
+    "Sage": "has an Ice element and heals", 
+    "Chamber": "Sharpshooter that can teleport"
+}
+
+print(val_agents)
