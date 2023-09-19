36a2f2df4506edf4e1bd9e49adef6c2dc7e38110
Joshua Lopez <joshualopez@Joshuas-MacBook-Air.local>
2023-09-18 22:16:34 -0700

    new new

---
+++ Module1Week1Assignment.py
@@ -0,0 +1,57 @@
+'''
+ 1. Define the following terms
+
+Statement- an instruction that the Python interpreter can execute
+Expression - A combination of operands and operators
+Literal - a data type and can hold any value type, such as strings, numbers, and more
+Variable - is a name given to a memory location
+Syntax - the structure of the language
+Boolean - true or false
+'''
+'''
+2. What are the results of the following Python expressions?
+'''
+2 ** 5 # 32
+float(10) # 10.0
+4//3 #1
+int(4/3) #1
+round(4/3, 2) # 1.33
+True and True and True and True and False #False
+False or False or False or False or True #True
+(False and False) or True #True
+ 
+
+# 3.  Write Python expressions to calculate the following (also include the results)
+
+# The number of seconds in one day
+60 * 60 * 24 #86400
+
+The area of a circle with a diameter of 12
+45 degrees to radians
+The remainder when you divide 123456789 by 123
+Infinity divided by infinity
+(Hint: you may need to import the math module for some of the above) 
+ 
+
+4. Given the following variables
+
+price = 4.99
+qty = 6
+shipping = 10.00
+sales_tax = 0.09
+Write two expressions and the results for:
+
+Just the total (don't include shipping or the tax)
+Total with shipping and tax (note that the shipping fee is not included in the tax calculation)
+Hint: use the round() to show the result with 2 significant places
+ 
+
+5.  Write Boolean expressions for the following
+
+n is a number that is greater than 0 and is not an odd number
+temperature range check for a cell phone. The temperature (temp) should be between 32 and 95 Fahrenheit.
+
+a filter for a house search. It should have at least 3 rooms and a garage. Let room be a numeric variable with garage is a Boolean variable
+
+medical prescreening. The patient must not be coughing, has not travelled out of the country within two weeks and has not tested positive for COVID. Suggested variable names: coughing, travel, covid
+'''
