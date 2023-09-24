fc9d9bca3c7539ac1902b00f4577b0e5a2fab069
Joshua Lopez <joshualopez@Joshuas-MacBook-Air.local>
2023-09-24 16:01:04 -0700

    week 2

---
+++ Module2Week2Assignment .py
@@ -0,0 +1,48 @@
+'''
+1. Strings
+
+a. Using string indexing, write two ways to get the character "P" from the string  "Python Programming"
+
+b. With string slice notation, extract the word "Programming" from the string  "Python Programming"
+
+2. String Methods
+
+For all of the questions below, assume the following:
+
+s = "Python Programming"
+
+a. Using string methods and the string s, write an expression to replace the substring "Programming" with "Software Development".
+
+b. What method can you use to verify that string s begins with the string "Python"?
+
+c. What method can you use to convert string s to all uppercase?
+
+d. Using operators and/or methods, write at least two different expressions to verify that the word "Python" appears in string s
+
+e. Write an expression to count the number of times the letter n appears in string s
+
+(Original Lists questions #3 and #4 moved to assignment #3)
+
+3. String Methods and Conditionals
+
+Using the input() function, read a string from the keyboard. Then using if/elif statements, print out the following messages based on the contents of the string you inputted:
+
+Input	Message
+uppercase letters only	"Uppercase"
+lowercase letters only	"Lowercase"
+letters only (mixed uppercase and lowercase)	"Letters"
+numbers only	"Numbers"
+whitespace only	"Spaces"
+other	"Other"
+4. String Formatting
+
+There are three players in a game. Their names and scores are listed below as variables and values respectively: 
+
+abe = 10
+bea = 12
+cecil = 5
+
+Write an f-string to display the total and the average of the scores. The output should look something like the following:
+
+Total Score: 27 Average Score: 9.0
+'''
